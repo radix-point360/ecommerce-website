b1c43aac39c22793a7ec2ec4c0b979f2a81e0b4c
sabir shaikh <saberpatel40@gmail.com>
2022-08-18 02:25:48 +0530

    added product section

--- ecommerce.js
+++ ecommerce.js
@@ -14,3 +14,20 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+
+// floating scrren starts
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+// flaoting screen ends
+
