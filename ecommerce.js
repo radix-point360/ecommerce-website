2a1a7f32fcb917d73261267faef06fe7540616c4
sabir shaikh <saberpatel40@gmail.com>
2022-08-17 22:51:51 +0530

    added carousel button

--- ecommerce.js
+++ ecommerce.js
@@ -1,41 +1,16 @@
-const leftArrow = document.querySelector("#click-left");
-const rightArrow = document.querySelector("#click-right");
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-var left = 1;
-var right = 5;
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
-let show = () => {
-  for (i = left; i <= right; i++ ) {
-    document.querySelector("#c" + i).style.display = "inline-block";
-  }
-};
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
 
-leftArrow.addEventListener("click", () => {
-  if (left <= 6 && right <= 9) {
-    document.querySelector("#c" + left).style.display = "none";
-    left += 1;
-    right += 1;
-    
-    for (i = left; i <= right; i++ ) {
-        document.querySelector("#c" + i).style.display = "inline-block";
-      }
-    
-  } else return;
-});
-
-rightArrow.addEventListener("click", () => {
-
-
-  if (left >= 4 && right >= 8) {
-    document.querySelector("#c" + left).style.display = "none";
-    left -= 1;
-    right -= 1;
-
-    for (i = left; i <= right; i++ ) {
-      document.querySelector("#c" + i).style.display = "inline-block";
-    } 
-    // show();
-  } else return;
-});
-
-
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
